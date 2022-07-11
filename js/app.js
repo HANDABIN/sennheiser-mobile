f0b2b339a036b70dbf4425525e271a1ad7619e16
HANDABIN <105402278+HANDABIN@users.noreply.github.com>
2022-07-11 15:23:51 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ $(function(){
   $('body').append(html); 
 
 }); // $
+
+
 $(function(){
 
   /*** #gnb toggle ***/
@@ -49,61 +51,3 @@ $(function(){
 
 
 
-
-
-
-// 제품 데이터 가져오기
-function getData() {
-  //
-  const DataURL = 'https://raw.githubusercontent.com/HANDABIN/sennheiser-mobile/main/data.json';
-  fetch(DataURL)
-  .then(function(res){
-    return res.json();//JASON 객체 변환
-  })
-  .then(function(obj){
-    showProducts(obj);
-    console.log(obj);
-  });
-}
-
-function showProducts(obj) {
-  // 현재 페이지의 URL query 파라미터(매개변수)
-  const query = location.search;
-  console.log(query);
-  // ? URL query문을 object(변수)로 변경
-  let params = new URLSearchParams(query).get('category');
-
-  // params == null 이면(시작 페이지 dog 출력)
-  if(params == null) {
-    params = '무선헤드폰'
-  }
-  console.log(params);  
-
-  // 동물 데이터 출력
-  obj.forEach(function(animal){
-    // 카테고리 구분 dog | cat | bird
-    // 요청한 params와 동물카테고리명이 일치하는 데이터만 출력
-    if(params == animal.category) {
-      let html = `
-      <div class="col">
-          <img src=${animal.imgUrl} alt="dog01">
-          <p class="name">${animal.name}</p>
-      </div>
-    `
-    $('.row').append(html);
-    }
-
-  });
-}
-
-
-
-
-/* 파라미터를 전달하여 요청하기
-  홈페이지주소?name=홍길동
-    매개변수(URL parameter) name = '홍길동'
-
-  index.html -> index.html?category=dog -> 개 보여주시오
-  cat.html -> cat.html?category=cat -> 고양이 보여주시오
-  bird.html -> bird.html?category=bird -> 새 보여주시오
-*/
